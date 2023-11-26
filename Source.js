c9fe2c6e8f2d3e10d597f676b3fe76d68a2144d8
Sonadas21 <dass21656@gmail.com>
2023-11-26 23:50:25 +0530

    Folder is pushed

---
+++ Source.js
@@ -0,0 +1,61 @@
+// var a=10;
+// let b = 20;
+// const c = 30;
+
+// console.log(a+"\n "+b+" "+c);
+
+// if(1 === true){console.log("True");}else{console.log("False");}
+
+// for(let i=0; i<5;i++){
+//     console.log(i+1);
+// }
+
+// while(true){
+//     console.log("Bye");
+// }
+
+// let bd = false;
+
+// while(bd){
+//     console.log("Bye");
+    
+// }
+// console.log("Sona");
+
+// let type = "Array";
+
+// switch(type){
+//     case "string":
+//         console.log("String");
+//         break;
+//     case "number":
+//         console.log("Number");
+//         break;
+//     case "boolean":
+//         console.log("Boolean");
+//         break;
+//     default:
+//         console.log("Default");
+//         break;
+// }
+
+//printing. the user input.......
+
+// let m = prompt("please enter a number");
+// alert("The number is : "+m);
+// document.write("The number is : "+m);
+
+// let n = "Sona Das";
+// // document.getElementById("ide").innerHTML = n;
+// document.write("Write up.............");
+// document.getElementById("ide").innerHTML="Sona Das";
+
+// let str = "";
+// let b = false;
+
+// for(let i=0; i<5;i++){
+//     str += `<li>${i}</li>`;
+// }
+
+// document.getElementById("ide").innerHTML = str;
+
